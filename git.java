a329af9eaca77345a12c5a832ca6da4ac0d507fb
Franruval <francisco.ruvalcaba8006@alumnos.udg.mx>
2024-05-08 12:21:34 -0600

    Agregué git.java, primer commit

---
+++ git.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class git {
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+
+        a.add(1);
+
+        System.out.println("num = " + a.get(0));
+    }
+}
