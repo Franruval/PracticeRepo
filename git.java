3896742fbcad55721282b88d44f23a8ff5d02ae1
Franruval <francisco.ruvalcaba8006@alumnos.udg.mx>
2024-05-08 12:47:40 -0600

    cambio a git.java

--- git.java
+++ git.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
 public class git {
-    public static void main(String[] args) {
-        List<Integer> a = new ArrayList<>();
+    static List<Integer> a = new ArrayList<>();
 
+    public static void main(String[] args) {
         a.add(1);
+        a.add(2);
 
         System.out.println("num = " + a.get(0));
     }
